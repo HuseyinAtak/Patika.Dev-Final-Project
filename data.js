1a9770024bc0b9db0b55c876d978cd7a0b231b66
Hüseyin Atak <huseyin.atak@outlook.com.tr>
2022-11-05 02:06:18 +0300

    I prepared http methods

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = function(){
+    return{
+        products:[
+            {id:1, name:"Damacana", price:35, imageUrl:'https://picsum.photos/200/300?random=7', description:"19Lt Damacana" },
+            {id:2, name:"Tup", price:34, imageUrl:'https://picsum.photos/200/300?random=1', description:"tupumuz " },
+            {id:3, name:"Su", price:33, imageUrl:'https://picsum.photos/200/300?random=2', description:"19Lt suyumuz" },
+            {id:4, name:"Pompa", price:44, imageUrl:'https://picsum.photos/200/300?random=3', description:"19Lt Damacana icin su pompasi" },
+            {id:5, name:"Hortum", price:443, imageUrl:'https://picsum.photos/200/300?random=4', description:"Tup icin aparat" },
+            {id:6, name:"Gaz", price:23, imageUrl:'https://picsum.photos/200/300?random=5', description:"gaz kucuk tup" }
+        ],
+        categories:[
+            {id:1, name:"Su"},
+            {id:2, name:"Tup"},
+            {id:3, name:"Diger"},
+        ],
+        orders:[
+
+        ]
+    }
+}
